0d35b8d1c98d49bf2618f5386283cb668c98a2fe
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-07-10 06:33:46 -0500

    fix: :bug: Solucion de error de envio de data nula
    se hace una validacion para detectar cuando los datos son nulos y los reemplaza con NULL a la hora de enviar, ademas se elimina el valor "id" aprovechando de la primary key es autoincrementable

--- routers/postBodegas.js
+++ routers/postBodegas.js
@@ -14,11 +14,20 @@ storagePostBodegas.use((req,res,next)=>{
  //Para ejecutar este endpoint copia la url de arriba y cambia los espacios en blanco por la data correspondiente. recuerda que el id debe ser un numero unico que no este en la tabla principal
  // y los valores id_responsable, created_by, update_by son datos forreanos de la tabla USERS
 storagePostBodegas.post('/', (req,res)=>{
-    const { id, nombre, id_responsable, estado, created_by, update_by, created_at, updated_at, deleted_at} = req.query;
+    const { nombre, id_responsable, estado, created_by, update_by, created_at, updated_at, deleted_at} = req.query;
     console.log(req.query);
     con.query(
-        /*SQL*/`INSERT INTO bodegas(id, nombre, id_responsable, estado, created_by, update_by, created_at, updated_at, deleted_at) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-        [id, nombre, id_responsable, estado, created_by, update_by, created_at, updated_at, deleted_at],
+        /*SQL*/`INSERT INTO bodegas(nombre, id_responsable, estado, created_by, update_by, created_at, updated_at, deleted_at) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`,
+        [
+            nombre, 
+            id_responsable, 
+            estado, 
+            created_by === "" ? null : created_by, 
+            update_by === "" ? null : update_by, 
+            created_at === "" ? null : created_at, 
+            updated_at === "" ? null : updated_at, 
+            deleted_at === "" ? null : deleted_at
+        ],
         (err,data,fil)=>{
             if (err) {
                 return res.status(500).send('Error al guardar los datos');
