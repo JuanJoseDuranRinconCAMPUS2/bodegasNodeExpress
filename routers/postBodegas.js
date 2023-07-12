7740b8de4f334836dbcb8da7c16e2bc26d1a272d
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-07-12 06:30:20 -0500

    feat: :bug: Correcion de error y envio de data funcional
    se corrige el error del objeto cambiando nombre de variables y con esto se logra hacer el end point con validacion de tipo de dato

--- routers/postBodegas.js
+++ routers/postBodegas.js
@@ -15,16 +15,15 @@ postBodegas.use((req,res,next)=>{
  //Para ejecutar este endpoint copia la url de arriba y cambia los espacios en blanco por la data correspondiente. recuerda que el id debe ser un numero unico que no este en la tabla principal
  // y los valores id_responsable, created_by, update_by son datos forreanos de la tabla USERS
  postBodegas.post('/', proxyPBodegas,(req,res)=>{
-    const obj = req.body;
-    const {id, nombre, id_responsable, estado, created_by, update_by } = obj;
+    const {Identification, name_B, identifaction_R, status, created_B, update_B} = req.body;
     con.query(
         /*SQL*/`INSERT INTO bodegas(nombre, id_responsable, estado, created_by, update_by, created_at, updated_at, deleted_at) VALUES(?, ?, ?, ?, ?, CURRENT_TIMESTAMP, NULL, NULL)`,
         [
-            nombre, 
-            id_responsable, 
-            estado, 
-            created_by, 
-            update_by
+            name_B, 
+            identifaction_R, 
+            status, 
+            created_B, 
+            update_B
         ],
         (err,data,fil)=>{
             if (err) {
